a43e4b103e61c77f16dba01a6f384a1214937b9d
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-04-26 15:19:00 +0530

    word count function using maturin

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,17 @@ fn say_hi(name :  String) -> String {
     format!("Hi there, {}!", name)
 }
 
+#[pyfunction]
+fn word_count(text :  String) -> usize {
+    text.split_whitespace().count()
+}
+
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn sample_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(compute_sum, m)?)?;
     m.add_function(wrap_pyfunction!(say_hi, m)?)?;
+    m.add_function(wrap_pyfunction!(word_count, m)?)?;
     Ok(())
 }
--- test.py
+++ test.py
@@ -5,3 +5,6 @@ print(sum_)
 
 greeting = sample_module.say_hi("Sid")
 print(greeting)
+
+word_count = sample_module.word_count("Hi, do you know the word count for this?")
+print(f"The word count is : {word_count}")
