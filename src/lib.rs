0467ecff5fe3b3ff311c8a50729a5a2073260fba
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-04-26 15:11:37 +0530

    rust and python with maturin

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,15 @@ fn compute_sum(a: i64, b: i64) -> PyResult<i64> {
     Ok((a + b))
 }
 
+#[pyfunction]
+fn say_hi(name :  String) -> String {
+    format!("Hi there, {}!", name)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn sample_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(compute_sum, m)?)?;
+    m.add_function(wrap_pyfunction!(say_hi, m)?)?;
     Ok(())
 }
--- test.py
+++ test.py
@@ -2,3 +2,6 @@ import sample_module
 
 sum_ = sample_module.compute_sum(10, 1)
 print(sum_)
+
+greeting = sample_module.say_hi("Sid")
+print(greeting)
